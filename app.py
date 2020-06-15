3d29b99be2223b20d5c9822a4279b35154879a28
Najoka Bungarus <ajgoe@gmx.de>
2020-06-15 15:53:50 +0200

    changed db import in app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 
-from db import db
-
 from resources.item import Item, ItemList
 
 from resources.store import Store, StoreList
